5b0ff59c1a614b44deb4bb78675b9c4b6736feb5
Erik Trygg <etrg@novonordisk.com>
2023-12-07 16:00:10 +0100

    Cleaned up the category model.

--- src/simple_animl/models/category.py
+++ src/simple_animl/models/category.py
@@ -1,8 +1,24 @@
+from __future__ import annotations
+
+from typing import Optional
+from .core import Field
 from .core import XmlModel
 
 
 class Category(XmlModel):
-    """Set of Tag elements."""
+    """
+    Defines a category of Parameters and SeriesSets. Used to model hierarchies.
 
-    # TBD
-    pass
+    Args:
+        name (str): Plain-text name of this item.
+        id (str): Anchor point for digital signature. This identifier is referred \
+              to from the "Reference" element in a Signature. Unique per document.
+
+    """
+
+    name: str = Field.Attribute()
+    id: Optional[str] = Field.Attribute()
+
+    # parameters: list[Parameter] = Field.Child()
+    # series_sets: list[SeriesSet] = Field.Child()
+    sub_categories: list[Category] = Field.Child()
